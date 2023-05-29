ce0c795f13a4646a67cf37778fc675b8893d2694
Amanda <amandaoliveirapolari@gmail.com>
2023-05-29 16:43:10 -0300

    Relembrando inputs controlados

--- src/components/ProfileForm/ProfileForm.js
+++ src/components/ProfileForm/ProfileForm.js
@@ -8,6 +8,13 @@ import {
 } from './ProfileForm.styled';
 
 const ProfileForm = () => {
+    // INPUTS CONTROLADOS | 3 PASSOS:
+    // 1. CRIAR OS ESTADOS PARA CADA INPUT
+    // 2. CRIAR AS FUNÇÕES QUE IRÃO MUDAR OS ESTADOS A PARTIR DE UM ONCLICK
+    // 3. CRIAR UM ONCHANGE PARA CADA INPUT E PASSAR PARA DENTRO DELAS A FUNÇÃO QUE FAZ AS MUDANÇAS SETANDO O ESTADO
+    // 4. O VALUE DE CADA INPUT SERA O VALOR DO ESTADO
+    // ------------------------------------------
+
     return (
         <FormWrapper>
             <h1>Edit your profile</h1>
